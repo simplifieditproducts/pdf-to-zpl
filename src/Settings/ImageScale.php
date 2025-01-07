c455e79dca0b04c14c77432dc60d1d71013c7f89
Ben Faerber <benfaerber@protonmail.com>
2025-01-07 12:29:37 -0700

    Doc comment + benchmark

--- src/Settings/ImageScale.php
+++ src/Settings/ImageScale.php
@@ -3,8 +3,20 @@
 namespace Faerber\PdfToZpl\Settings;
 
 enum ImageScale {
+    /** 
+    * Scale the Image to fill all available space 
+    * (does not respect aspect ratio) 
+    */ 
     case Fill;
+
+    /** 
+    * Scale the Image to fill the most available space while respecting aspect ratio 
+    */ 
     case Cover;
+
+    /**
+    * Do not scale the image in anyway
+    */ 
     case None;
 
     public function shouldResize(): bool {
