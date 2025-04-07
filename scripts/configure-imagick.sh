7f031d0603b64a3b0ca6c66b4dbafcd634487c9f
Ben Faerber <benfaerber@protonmail.com>
2025-04-06 22:33:47 -0600

    Remove weird dolar signs

--- scripts/configure-imagick.sh
+++ scripts/configure-imagick.sh
@@ -8,10 +8,10 @@ policy_file="$image_magick_folder/policy.xml"
 echo $policy_file;
 sed -i 's@rights="none" pattern="PDF"@rights="read | write" pattern="PDF"@g' "$policy_file"
 
-echo $'New Version of File:\n'
+echo 'New Version of File:\n'
 cat $policy_file
 
-echo $'\nChange Made:'
+echo '\nChange Made:'
 cat $policy_file | grep "pattern=\"PDF\""
 
-echo $'\nNo Errors Detected! You now should be able to modify PDFs'
+echo '\nNo Errors Detected! You now should be able to modify PDFs'
